9aee74204db487c99d13b09cf907348e8fe4e326
Moritz Schillinger <28610231+schilli91@users.noreply.github.com>
2021-12-06 00:57:43 +0100

    Day 4 not finished

--- pkg/day_4/day_4.go
+++ pkg/day_4/day_4.go
@@ -4,7 +4,60 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/schilli91/aoc2021/pkg/common"
 )
+
+type BingoCard struct {
+	rows   [5][5]string
+	checks [5][5]bool
+}
+
+func NewBingoCard(rows [5]string) BingoCard {
+	b := *new(BingoCard)
+	for i, row := range rows {
+		row = strings.ReplaceAll(strings.TrimSpace(row), "  ", " ")
+		b.rows[i] = *(*[5]string)(strings.Split(row, " "))
+	}
+
+	return b
+}
+
+func (card *BingoCard) checkCard(value string) {
+	for i := 0; i < len(card.rows); i++ {
+		for j := 0; j < len(card.rows[i]); j++ {
+			if value == card.rows[i][j] {
+				card.checks[i][j] = true
+			}
+		}
+	}
+}
+
+func (card BingoCard) Bingo() bool {
+	colBingo := []bool{true, true, true, true, true}
+
+	for row := range card.checks {
+		rowBingo := true
+		for col, item := range card.checks[row] {
+			if !item {
+				rowBingo = false
+				colBingo[col] = false
+			}
+		}
+		if rowBingo {
+			return true
+		}
+	}
+	for _, bingo := range colBingo {
+		if bingo {
+			return true
+		}
+	}
+
+	return false
+}
 
 func Run() {
 	day := 4
@@ -15,11 +68,75 @@ func Run() {
 	}
 	defer data.Close()
 
-	score := PlayBingo(data)
+	values, cards := LoadBingoCards(data)
+
+	score := PlayBingo(values, cards)
 	fmt.Printf("The final Bingo score is %d\n", score)
 }
 
-func PlayBingo(data *os.File) int {
+func LoadBingoCards(data *os.File) ([]string, []BingoCard) {
+	var values []string
+	var cards []BingoCard
 
+	lines := common.ReadLinesString(data)
+	counter := 0
+	for i, line := range lines {
+		if i == 0 {
+			values = strings.Split(line, ",")
+			continue
+		}
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+		if counter > 0 {
+			counter--
+			continue
+		}
+		rows := [5]string{
+			lines[i],
+			lines[i+1],
+			lines[i+2],
+			lines[i+3],
+			lines[i+4],
+		}
+		c := NewBingoCard(rows)
+		cards = append(cards, c)
+		counter = 4
+	}
+
+	return values, cards
+}
+
+func (card BingoCard) finalScore(lastValue int) int {
+	total := 0
+	for i := 0; i < len(card.checks); i++ {
+		for j := 0; j < len(card.checks[i]); j++ {
+			if !card.checks[i][j] {
+				item, err := strconv.Atoi(card.rows[i][j])
+				if err != nil {
+					log.Fatal(err)
+				}
+				total += item
+			}
+		}
+
+	}
+	return 0
+}
+
+func PlayBingo(values []string, cards []BingoCard) int {
+
+	for _, card := range cards {
+		for _, value := range values {
+			card.checkCard(value)
+			if card.Bingo() {
+				lastValue, err := strconv.Atoi(value)
+				if err != nil {
+					log.Fatal(err)
+				}
+				return card.finalScore(lastValue)
+			}
+		}
+	}
 	return 0
 }
--- pkg/day_4/day_4_test.go
+++ pkg/day_4/day_4_test.go
@@ -7,17 +7,109 @@ import (
 )
 
 func TestDay4(t *testing.T) {
-	t.Run("test", func(t *testing.T) {
+	t.Run("test play bingo", func(t *testing.T) {
 		data, err := os.Open("test_data.txt")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer data.Close()
 
-		got := PlayBingo(data)
+		values, cards := LoadBingoCards(data)
+
+		got := PlayBingo(values, cards)
 		want := 4512
 		if want != got {
 			t.Errorf("Incorrect value: got %d, want %d\n\n", got, want)
+		}
+	})
+	t.Run("test bingo card", func(t *testing.T) {
+		r := [5]string{
+			"22 13 17 11  0",
+			"8  2 23  4 24",
+			"21  9 14 16  7",
+			"6 10  3 18  5",
+			"1 12 20 15 19",
+		}
+		b := NewBingoCard(r)
+		got := b.rows
+
+		w := [5][5]string{
+			{"22", "13", "17", "11", "0"},
+			{"8", "2", "23", "4", "24"},
+			{"21", "9", "14", "16", "7"},
+			{"6", "10", "3", "18", "5"},
+			{"1", "12", "20", "15", "19"},
+		}
+		want := w
+		if want != got {
+			t.Errorf("Incorrect value: got %v, want %v\n\n", got, want)
+		}
+	})
+	t.Run("test bingo, column", func(t *testing.T) {
+		b := new(BingoCard)
+		// b.checks=[5][5]bool{
+		// 	{false, false, false, false, false},
+		// 	{false, false, false, false, false},
+		// 	{false, false, false, false, false},
+		// 	{false, false, false, false, false},
+		// 	{false, false, false, false, false},
+		// }
+		b.checks = [5][5]bool{
+			{false, true, false, false, false},
+			{false, true, false, false, false},
+			{false, true, false, false, false},
+			{false, true, false, false, false},
+			{false, true, false, false, false},
+		}
+		got := b.Bingo()
+		want := true
+		if want != got {
+			t.Errorf("Incorrect value: got %v, want %v\n\n", got, want)
+		}
+	})
+	t.Run("test bingo, row", func(t *testing.T) {
+		b := new(BingoCard)
+		b.checks = [5][5]bool{
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{true, true, true, true, true},
+			{false, false, false, false, false},
+		}
+		got := b.Bingo()
+		want := true
+		if want != got {
+			t.Errorf("Incorrect value: got %v, want %v\n\n", got, want)
+		}
+	})
+	t.Run("test bingo, no bingo", func(t *testing.T) {
+		b := new(BingoCard)
+		b.checks = [5][5]bool{
+			{false, false, true, false, true},
+			{true, true, false, false, false},
+			{false, false, true, false, true},
+			{true, true, false, true, false},
+			{true, true, true, false, false},
+		}
+		got := b.Bingo()
+		want := false
+		if want != got {
+			t.Errorf("Incorrect value: got %v, want %v\n\n", got, want)
+		}
+	})
+	t.Run("test bingo, no bingo empty card", func(t *testing.T) {
+		b := new(BingoCard)
+		b.checks = [5][5]bool{
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+			{false, false, false, false, false},
+		}
+		got := b.Bingo()
+		want := false
+		if want != got {
+			t.Errorf("Incorrect value: got %v, want %v\n\n", got, want)
 		}
 	})
 }
