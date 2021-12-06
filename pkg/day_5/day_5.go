3591fdca452246e34a1fa40efee8a17203d66cdb
Moritz Schillinger <28610231+schilli91@users.noreply.github.com>
2021-12-07 00:02:00 +0100

    Day 5 start

--- pkg/day_5/day_5.go
+++ pkg/day_5/day_5.go
@@ -4,7 +4,38 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/schilli91/aoc2021/pkg/common"
 )
+
+type Point struct {
+	x int
+	y int
+}
+
+type Line struct {
+	start Point
+	end   Point
+}
+
+func (l Line) LinePoints() []Point {
+	start, end := l.start, l.end
+	if start.x == end.x && start.y == end.y {
+		return []Point{start}
+	}
+
+	if l.start.x > l.end.x || l.start.y > l.end.y {
+		start, end = end, start
+	}
+
+	return []Point{}
+}
+
+type VentsMap struct {
+	vents []Point
+}
 
 func Run() {
 	day := 5
@@ -16,4 +47,42 @@ func Run() {
 	defer data.Close()
 
 	fmt.Printf("Day %d\n", day)
+}
+
+func HydrothermalVents(data *os.File) int {
+
+	return 0
+}
+
+func LoadLines(data *os.File) []Line {
+	rows := common.ReadLinesString(data)
+
+	lines := []Line{}
+	for _, row := range rows {
+		lines = append(lines, NewLineFromStr(row))
+	}
+
+	return lines
+}
+
+func NewLineFromStr(s string) Line {
+	parts := strings.Split(strings.TrimSpace(s), "->")
+
+	start := NewPointFromStr(strings.TrimSpace(parts[0]))
+	end := NewPointFromStr(strings.TrimSpace(parts[1]))
+
+	return Line{start: start, end: end}
+}
+
+func NewPointFromStr(s string) Point {
+	p := strings.Split(strings.TrimSpace(s), ",")
+	x, err := strconv.Atoi(p[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	y, err := strconv.Atoi(p[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return Point{x: x, y: y}
 }
--- pkg/day_5/day_5_test.go
+++ pkg/day_5/day_5_test.go
@@ -7,17 +7,64 @@ import (
 )
 
 func TestDayX(t *testing.T) {
-	t.Run("test", func(t *testing.T) {
+	t.Run("test hydro thermal vents", func(t *testing.T) {
 		data, err := os.Open("test_data.txt")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer data.Close()
 
-		got := 0
-		want := 150
+		got := HydrothermalVents(data)
+		want := 5
 		if want != got {
 			t.Errorf("Incorrect value: got %d, want %d\n\n", got, want)
 		}
 	})
+	t.Run("test line from string", func(t *testing.T) {
+		s := "0,9 -> 5,9"
+
+		got := NewLineFromStr(s)
+
+		want := Line{
+			start: Point{x: 0, y: 9},
+			end:   Point{x: 5, y: 9},
+		}
+		if want != got {
+			t.Errorf("Incorrect value: got %v, want %v\n\n", got, want)
+		}
+	})
+	t.Run("test points from line", func(t *testing.T) {
+		s := "3,4 -> 1,4"
+		l := NewLineFromStr(s)
+		got := l.LinePoints()
+
+		want := []Point{
+			{x: 1, y: 4},
+			{x: 2, y: 4},
+			{x: 3, y: 4},
+		}
+
+		// fmt.Println(got)
+		// fmt.Println(want)
+
+		if !comparePointSlices(want, got) {
+			t.Errorf("Incorrect value: got %v, want %v\n\n", got, want)
+		}
+	})
+}
+
+func comparePointSlices(a []Point, b []Point) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		ai, bi := a[i], b[i]
+		if ai.x != bi.x {
+			return false
+		}
+		if ai.y != bi.y {
+			return false
+		}
+	}
+	return true
 }
